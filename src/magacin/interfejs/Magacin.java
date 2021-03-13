e446cca162615425c4850ab6248c4379304f503a
StefanK98 <kujovicstefan98@gmail.com>
2021-03-13 14:48:31 +0100

    Implementirane metode iz MagacinInterface-a(nije dovršeno)

---
+++ src/magacin/interfejs/Magacin.java
@@ -0,0 +1,20 @@
+package magacin.interfejs;
+
+import magacin.Artikal;
+
+public class Magacin implements MagacinInterface {
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+	}
+
+	@Override
+	public Artikal vratiArtikal(int sifra) {
+		return null;
+	}
+
+}
