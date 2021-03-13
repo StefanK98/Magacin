eed138424ca2a245a0517b1ba5bb6d763904f872
StefanK98 <kujovicstefan98@gmail.com>
2021-03-13 15:19:20 +0100

    Implementirana metoda dodajArtikal u klasi Magacin

--- src/magacin/interfejs/Magacin.java
+++ src/magacin/interfejs/Magacin.java
@@ -1,11 +1,24 @@
 package magacin.interfejs;
 
+import java.util.LinkedList;
+
 import magacin.Artikal;
 
 public class Magacin implements MagacinInterface {
+	
+	private LinkedList<Artikal> artikli=new LinkedList<Artikal>();
 
 	@Override
 	public void dodajArtikal(Artikal artikal) {
+		if(artikal==null)
+			return;
+		for (Artikal art : artikli) {
+			if(art.getSifra()==artikal.getSifra()) {
+				art.setKolicina(art.getKolicina()+1);
+				return;
+			}
+		}
+		artikli.add(artikal);
 	}
 
 	@Override
